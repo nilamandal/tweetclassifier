3046acc2b6b5c099bba4ea70a68a381eb75acf81
Aya <48328199+swazuz@users.noreply.github.com>
2019-10-13 13:51:37 -0400

    Create bag_of_words.py

---
+++ bag_of_words.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk 
+nltk.download('punkt')
+nltk.download('stopwords')
+
+import string
+import re
+'exec(%matplotlib inline)'
+pd.set_option('display.max_colwidth', 100)
+import preprocessor as p
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+
+def load_data():
+    #read data
+    data = pd.read_csv('political.csv',header=0,usecols = ['message','message:confidence','text'],encoding = 'unicode_escape')
+    preprocessed_tweets_text = []
+    print("Check before filter political data")
+    print(data.shape)
+    # Get only rows that has (political or attack)
+    data = filter_support_vs_attack(data)
+
+    # preprocess text of tweets
+    for index, row in data.iterrows():
+        row["text"] = preprocess_tweet(row["text"])
+        preprocessed_tweets_text.append(row["text"])
+    
+    generate_bag_of_words(preprocessed_tweets_text)
+    return data
+
+def preprocess_tweet(text):
+
+    # Check characters to see if they are in punctuation
+    nopunc = [char for char in text if char not in string.punctuation]
+    # Join the characters again to form the string.
+    nopunc = ''.join(nopunc)
+    # convert text to lower-case
+    nopunc = nopunc.lower()
+    # remove URLs
+    nopunc = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))', '', nopunc)
+    nopunc = re.sub(r'http\S+', '', nopunc)
+    # remove usernames
+    nopunc = re.sub('@[^\s]+', '', nopunc)
+    # remove the # in #hashtag
+    nopunc = re.sub(r'#([^\s]+)', r'\1', nopunc)
+    # remove repeated characters
+    nopunc = word_tokenize(nopunc)
+    # remove stopwords from final word list
+    word_list = [word for word in nopunc if word not in stopwords.words('english')] 
+    return ' '.join(word_list)
+
+def filter_support_vs_attack(dataframe):
+    filtered_dataframe = dataframe[dataframe.message.isin(['support', 'attack'])]
+    print("Check filtered political data")
+    print(filtered_dataframe.shape)
+    return filtered_dataframe
+
+def generate_bag_of_words(tweets_text):
+    #print('All Tweets')
+    #print(tweets_text)
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(tweets_text)
+    print('Bag of words Vectors for each Tweet')
+    print(len(X.toarray()))
+    print(X.toarray())
+    print("Tweet words and its frequency in all tweets")
+    #print(vectorizer.vocabulary_ )
+
+tweet_df = load_data()
+#print(tweet_df.head())
+
+
+
+
